9a08517b36712cdc27d6ffbdd96821d7fcf0d860
highmountain12321 <timesaver1006@gmail.com>
2023-10-20 18:31:19 -0300

    fix: change text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Link href="/contacts">
-        <h6>Go to Contacts -></h6>
+        <h6>{"Go to Contacts ->"}</h6>
       </Link>
     </main>
   )
