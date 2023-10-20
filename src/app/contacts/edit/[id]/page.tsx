c2c1b089b7fcf4b80ed5976d3bc149337466700e
highmountain12321 <timesaver1006@gmail.com>
2023-10-20 13:37:24 -0300

    feat: bg-white

--- src/app/contacts/edit/[id]/page.tsx
+++ src/app/contacts/edit/[id]/page.tsx
@@ -9,8 +9,10 @@ export default async function Page({params}: {params: {id: string}}){
     console.log("id is : ", id)
     const contact = await getContactById(Number(id))
     return (
-        <div className="w-full h-full">
-            <h1>Contact List App</h1>
+        <div className="w-full h-screen bg-white">
+            <div className="flex justify-center items-center p-4">
+                <h1 className="text-3xl font-bold">Edit Contacts</h1>
+            </div>
             <ContactForm contact={contact.data.data} isEditing={true} />
         </div>
     )
--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,17 +1,47 @@
-import Link from "next/link"
+"use client"
+import React, { useState } from "react";
+import Link from "next/link";
+import ContactList from "@/components/ContactList";
 
-import ContactList from "@/components/ContactList"
-import { getContacts } from "@/lib/api"
+function Page() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-export default async function Page(){
-    return (
-        <div className="w-full h-screen">
-            <div className="flex justify-between align-center p-4 border-b border-b-2 border-blue-500 bg-white items-center">
-                <Link href={'/'} className="text-2xl font-bold">Contact List</Link>
-                <Link href={"/contacts/add"} className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 mr-2">Add contacts</Link>
-            </div>
-            <ContactList />
-            <div id="modal-root"></div>
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className="w-full h-screen bg-white">
+      <div className="flex justify-between items-center p-4 border-b border-b-2 border-blue-500 bg-white ">
+        <div className="lg:hidden">
+          <button onClick={toggleMenu} className="block text-3xl">
+            ☰
+          </button>
         </div>
-    )
+        {/* Dropdown menu for mobile */}
+        {isMenuOpen && (
+            <div className="lg:hidden h-[60px] absolute left-[50px]">
+            <div className=" bg-blue-500 text-white border border-gray-300 m-2 p-2 rounded">
+                <Link href={"/contacts/add"} className="block text-white hover:underline">
+                Add contacts
+                </Link>
+            </div>
+            </div>
+        )}
+        <Link href={'/'} className="text-2xl font-bold">
+          Contact List
+        </Link>
+        {/* "Add contacts" button always visible on desktop */}
+        <div className="hidden lg:block">
+          <Link href={"/contacts/add"} className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 ml-2">
+            Add contacts
+          </Link>
+        </div>
+      </div>
+      <ContactList />
+      <div id="modal-root"></div>
+    </div>
+  );
 }
+
+export default Page;
