1ed1766d7c87c7bcdea94760b0de8173b2cb2b6f
highmountain12321 <timesaver1006@gmail.com>
2023-10-19 23:58:32 -0300

    feat: edit contact functionality TODO:// define edit on the parent

--- src/components/ContactItem.tsx
+++ src/components/ContactItem.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+"use client"
+import { updateContact } from '@/lib/api';
+import React, {useState} from 'react';
 
 interface ContactProps {
     contact: {
         id: number,
-        firstName: string,
-        lastName: string,
+        first_name: string,
+        last_name: string,
         job: string,
         description: string
     },
@@ -12,14 +14,65 @@ interface ContactProps {
 }
 
 const ContactItem: React.FC<ContactProps> = ({ contact, onDelete }) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedContact, setEditedContact] = useState(contact);
+    
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setEditedContact(prevState => ({ ...prevState, [name]: value }));
+    };
+    
+    const handleEditSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        // Call the API to update the contact, assuming you've a function called 'updateContact'
+        await updateContact(editedContact.id, editedContact);
+        setIsEditing(false);
+    };
+    
     return (
         <div>
-            <h3>{contact.firstName} {contact.lastName}</h3>
-            <p>{contact.job}</p>
-            <p>{contact.description}</p>
-            <button onClick={() => onDelete(contact.id)}>Delete</button>
+            {isEditing ? (
+                <form onSubmit={handleEditSubmit}>
+                    <input
+                        name="first_name"
+                        value={editedContact.first_name}
+                        onChange={handleInputChange}
+                        required
+                    />
+                    <input
+                        name="last_name"
+                        value={editedContact.last_name}
+                        onChange={handleInputChange}
+                        required
+                    />
+                    <input
+                        name="job"
+                        value={editedContact.job}
+                        onChange={handleInputChange}
+                        required
+                    />
+                    <input
+                        name="description"
+                        value={editedContact.description}
+                        onChange={handleInputChange}
+                        required
+                    />
+                    <button type="submit">Save</button>
+                    <button onClick={() => setIsEditing(false)}>Cancel</button>
+                </form>
+            ) : (
+                <>
+                    <h3>{contact.first_name} {contact.last_name}</h3>
+                    <p>{contact.job}</p>
+                    <p>{contact.description}</p>
+                    <button onClick={() => setIsEditing(true)}>Edit</button>
+                    <button onClick={() => onDelete(contact.id)}>Delete</button>
+                </>
+            )}
         </div>
     );
+
 }
 
 export default ContactItem;
