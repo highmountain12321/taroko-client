62213bb68bd46720cc5408456f19e06a35772fda
highmountain12321 <timesaver1006@gmail.com>
2023-10-20 06:53:38 -0300

    feat: basic imput

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -8,21 +8,21 @@ import { ContactType } from '@/db/types';
 const ContactList: React.FC = () => {
     const [contacts, setContacts] = useState<any[]>([]);
     const [newContact, setNewContact] = useState<ContactType>({
-        id:-1,
+        id: -1,
         first_name: '',
         last_name: '',
         job: '',
         description: ''
     });
-    const [editingId, setEditingId] = useState<number|null>(null);
+    const [editingId, setEditingId] = useState<number | null>(null);
     const startEditing = (id: number) => {
         setEditingId(id);
     };
-    
+
     const stopEditing = () => {
         setEditingId(null);
     };
-    
+
     useEffect(() => {
         loadContacts();
     }, []);
@@ -36,7 +36,7 @@ const ContactList: React.FC = () => {
         await deleteContact(id);
         loadContacts();
     };
-    const handleEdit =async (id:number, editedContact: ContactType) => {
+    const handleEdit = async (id: number, editedContact: ContactType) => {
         await updateContact(editedContact.id, editedContact);
         loadContacts();
         stopEditing();
@@ -45,7 +45,7 @@ const ContactList: React.FC = () => {
         const { name, value } = e.target;
         setNewContact(prevState => ({ ...prevState, [name]: value }));
     };
-    
+
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         // Call the API to add the contact, assuming you've a function called 'addContact'
@@ -60,16 +60,17 @@ const ContactList: React.FC = () => {
             description: ''
         });
     };
-    
+
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+            <form onSubmit={handleSubmit} className="bg-white p-4 rounded shadow mb-4">
                 <input
                     name="first_name"
                     value={newContact.first_name}
                     onChange={handleInputChange}
                     placeholder="First Name"
                     required
+                    className="w-full p-2 mb-2 border rounded"
                 />
                 <input
                     name="last_name"
@@ -77,6 +78,7 @@ const ContactList: React.FC = () => {
                     onChange={handleInputChange}
                     placeholder="Last Name"
                     required
+                    className="w-full p-2 mb-2 border rounded"
                 />
                 <input
                     name="job"
@@ -84,6 +86,7 @@ const ContactList: React.FC = () => {
                     onChange={handleInputChange}
                     placeholder="Job"
                     required
+                    className="w-full p-2 mb-2 border rounded"
                 />
                 <input
                     name="description"
@@ -91,19 +94,27 @@ const ContactList: React.FC = () => {
                     onChange={handleInputChange}
                     placeholder="Description"
                     required
+                    className="w-full p-2 mb-2 border rounded"
                 />
-                <button type="submit">Add Contact</button>
+                <button type="submit" className="bg-blue-500 text-white p-2 rounded hover:bg-blue-700">Add Contact</button>
             </form>
 
             {/* Existing Contact List */}
-            {contacts.length === 0 ? (
-                <div>No content</div>
-            ) : (
-                contacts.map((contact) => (
-                    <ContactItem key={contact.id} contact={contact} isEditing={contact.id == editingId } onEdit={handleEdit} startEditing={startEditing} stopEditing={stopEditing} onDelete={handleDelete} />
-                ))
-            )}
+            <div className="bg-white p-4 rounded shadow">
+                {contacts.length === 0 ? (
+                    <div>No content</div>
+                ) : (
+                    <div className="grid gap-4">
+                        {contacts.map((contact) => (
+                            <ContactItem key={contact.id} contact={contact} isEditing={contact.id == editingId} onEdit={handleEdit} startEditing={startEditing} stopEditing={stopEditing} onDelete={handleDelete} />
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
+
+
+
 
     );
 }
