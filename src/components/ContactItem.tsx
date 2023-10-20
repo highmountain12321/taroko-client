0988c308bd0e8c436080e70ca31ecddd93ca94d1
highmountain12321 <timesaver1006@gmail.com>
2023-10-20 11:08:19 -0300

    style: contact item component

--- src/components/ContactItem.tsx
+++ src/components/ContactItem.tsx
@@ -17,20 +17,29 @@ const ContactItem: React.FC<ContactProps> = ({ contact, onDelete }) => {
     const router = useRouter()
     return (
         <div className="border p-4 rounded shadow">
-            <img
-                className='w-16 h-16 rounded-full object-cover mb-4'
-                src={"/download.jpg"}
-                alt={`${contact.first_name}`}
-            />
-            <h2 className="text-lg font-semibold mb-2">{contact.first_name} {contact.last_name}</h2>
-            <p className="text-gray-600 mb-4">Job: {contact.job}</p>
-            <p className="mb-4">Description: {contact.description}</p>
+            <div className="flex justify-between">
+                <div className="flex items-center gap-4">
+                    <img
+                        className='w-16 h-16 rounded-full object-cover mb-4'
+                        src={"/download.jpg"}
+                        alt={`${contact.first_name}`}
+                    />
+                    <h2 className="text-lg font-semibold mb-2">{contact.first_name} {contact.last_name}</h2>
 
-            <div className="flex">
-                {/* <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 mr-2" onClick={() => startEditing(contact.id)}>Edit</button> */}
-                <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 mr-2" onClick={() => { router.push(`/contacts/edit/${contact.id}`) }}>Edit</button>
-                <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700" onClick={() => onDelete(contact.id)}>Delete</button>
+                </div>
+                <div className="flex flex-col gap-2 mt-4">
+                    <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700" onClick={() => { router.push(`/contacts/edit/${contact.id}`) }}>Edit</button>
+                    <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700" onClick={() => onDelete(contact.id)}>Delete</button>
+                </div>
+
             </div>
+
+            <div className='ml-20'>
+                <p className="text-gray-600 mb-2">Job: {contact.job}</p>
+                <p className="mb-2">Description: {contact.description}</p>
+            </div>
+
+
 
         </div>
     );
