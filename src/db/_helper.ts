cbd6e17bf8d313360680b5fbd367d17a9ffec7bc
highmountain12321 <timesaver1006@gmail.com>
2023-10-21 12:14:21 -0300

    fix: updated json file name

--- src/db/_helper.ts
+++ src/db/_helper.ts
@@ -2,11 +2,11 @@ const fs = require('fs');
 const path = require('path')
 import { ContactType } from "./types";
 
-const filePath = path.join(process.cwd(), 'src/db', 'contacts.json')
+const filePath = path.join(process.cwd(), 'src/db', 'db_seed.json')
 
 // const contacts = JSON.parse(rawdata);
 
-export const getContacts = async ()=> {
+export const getContacts_from_file = async ()=> {
     try {
         const rawdata = await fs.readFileSync(filePath);
         return JSON.parse(rawdata) as ContactType[];
@@ -14,8 +14,12 @@ export const getContacts = async ()=> {
         return [] as ContactType[]
     }
 };
-let contacts;
-getContacts().then(results=> contacts = results)
+let contacts:ContactType[] = [];
+getContacts_from_file().then(results=> contacts = results)
+
+export const getContacts = async ()=> {
+    return contacts;
+}
 
 // export const writeContacts = async (contacts:ContactType[]) => await fs.writeFileSync(filePath, JSON.stringify(contacts));
 export const writeContacts = async (contact_lists:ContactType[]) => contacts = contact_lists;
